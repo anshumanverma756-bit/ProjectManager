e002735a67ee931660298dfe49d6bf7d526c6d00
Anshuman Verma <anshumanverma756@gmail.com>
2026-09-08 19:55:18 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import dotenv from "dotenv"
-import app from "./appp.js"
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./database/database.js"
 
 dotenv.config({
   path:"./.env",
@@ -8,7 +9,13 @@ dotenv.config({
 const port = process.env.PORT || 3000;
 
 
-app.listen(port, ()=>{
-  console.log(`Example app listening on port http://localhost:${port}`);
-});
-  
+connectDB()
+   .then(() => {
+     app.listen(port, ()=>{
+      console.log(`Example app listening on port http://localhost:${port}`);
+  });
+})
+  .catch((err) => {
+    console.error("MongoDB connection error", err);
+    process.exit(1);
+  })
