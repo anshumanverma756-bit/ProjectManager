c1275c39f25aa75ee79607f3d46132b94c59f477
anshu <anshumanverma756@gmail.com>
2026-09-14 21:43:35 +0530

    hashing the passwords with prehooks of mongooes

--- src/modles/user.modles.js
+++ src/modles/user.modles.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
 
 const userSchema = new Schema({
   avatar:{
@@ -57,5 +59,12 @@ const userSchema = new Schema({
   timestamps: true
  },
 );
+
+userSchema.pre("save", async function (next) {
+  if(!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+})
 
 export const User = mongoose.model("User", userSchema) 
