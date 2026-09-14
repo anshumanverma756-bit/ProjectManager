d7556db923b57fe17eaf20db8e4b6cc80e5d194b
anshu <anshumanverma756@gmail.com>
2026-09-14 21:53:05 +0530

    handling mongooes method for password verfication

--- src/modles/user.modles.js
+++ src/modles/user.modles.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-import bcrypt from "bcrypt";
+import brcypt from "brcypt";
 
 
 const userSchema = new Schema({
@@ -63,8 +63,12 @@ const userSchema = new Schema({
 userSchema.pre("save", async function (next) {
   if(!this.isModified("password")) return next();
 
-  this.password = await bcrypt.hash(this.password, 10);
+  this.password = await brcypt.hash(this.password, 10);
   next();
-})
+});
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await brcypt.compare(password, this.password);
+};
 
 export const User = mongoose.model("User", userSchema) 
