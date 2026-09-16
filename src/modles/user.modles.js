4284398ad660080f23623c840cc86359633b746c
anshu <anshumanverma756@gmail.com>
2026-09-16 22:55:21 +0530

    added refresh token and a temprory token using crypto

--- src/modles/user.modles.js
+++ src/modles/user.modles.js
@@ -1,7 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 import brcypt from "brcypt";
 import jwt from "jsonwebtoken";
-
+import crypto from "crypto";
 
 const userSchema = new Schema({
   avatar:{
@@ -73,7 +73,7 @@ userSchema.methods.isPasswordCorrect = async function (password){
 };
 
 userSchema.methods.generateAccessToken = function(){
-  jwt.sign({
+  return jwt.sign({
     _id:this._id,
     email: this.email,
     username: this.username
@@ -82,5 +82,25 @@ userSchema.methods.generateAccessToken = function(){
   {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
 )
 }
+userSchema.methods.generateRefreshToken = function(){
+  return jwt.sign({
+    _id:this._id,
+  },
+  process.env.REFRESH_TOKEN_SECRET,
+  {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+)
+}
+userSchema.methods.generateTemporaryToken = function(){
+  const unHashedToken = crypto.randomBytes(20).toString("hex")
+
+  const hashedToken = crypto
+    .createHash("sha256")
+      .update(unHashedToken)
+      .digest("hex")
+
+  const tokenExpiry = Date.now()  + (20*60*1000)
+  return {unHashedToken, hashedToken, tokenExpiry}
+    
+};
 
 export const User = mongoose.model("User", userSchema) 
