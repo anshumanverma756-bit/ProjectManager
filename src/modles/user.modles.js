6eb9d419c3b335ca7c79c75da09d5a1fecf755a7
anshu <anshumanverma756@gmail.com>
2026-09-16 22:29:01 +0530

    adding access token code part

--- src/modles/user.modles.js
+++ src/modles/user.modles.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import brcypt from "brcypt";
+import jwt from "jsonwebtoken";
 
 
 const userSchema = new Schema({
@@ -70,5 +71,16 @@ userSchema.pre("save", async function (next) {
 userSchema.methods.isPasswordCorrect = async function (password){
     return await brcypt.compare(password, this.password);
 };
+
+userSchema.methods.generateAccessToken = function(){
+  jwt.sign({
+    _id:this._id,
+    email: this.email,
+    username: this.username
+  },
+  process.env.ACCESS_TOKEN_SECRET,
+  {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+)
+}
 
 export const User = mongoose.model("User", userSchema) 
