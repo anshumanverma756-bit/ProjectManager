664261a55731534a2b943570e9070b424a55078a
anshu <anshumanverma756@gmail.com>
2026-09-17 21:34:44 +0530

    genrating the content for the email using Mailgen for eamil genration

---
+++ src/uitles/mail.js
@@ -0,0 +1,42 @@
+import Mailgen from "mailgen";
+
+const emailVerificationMailgenContent = (username, verificationUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "Welcome to our app! We are excited to have you.",
+      action: {
+        instructions: "To verify your email, please click on the following button.",
+        button: {
+          color: "#22BC66",
+          text: "Verify your email",
+          link: verificationUrl,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+};
+
+const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "We got a request to reset the password of your account",
+      action: {
+        instructions: "To reset your password click on the following button or link.",
+        button: {
+          color: "#22BC66",
+          text: "Reset password",
+          link: passwordResetUrl,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+};
+
+
+export default emailVerificationMailgenContent;
