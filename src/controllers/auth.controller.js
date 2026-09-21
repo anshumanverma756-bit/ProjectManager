8816d07fe0371eae4c0cf24f0ca9aa392fa87ac8
anshu <anshumanverma756@gmail.com>
2026-09-21 22:40:53 +0530

    add the some part of auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,24 +1,30 @@
-// import {User} from "../modles/user.models.js";
-// import {ApiResponse} from "../utils/api_response.js";
-// import {ApiError} from "../utils/api_error.js";
-// import {asyncHandler} from "../utils/async_handler.js";
+import { User } from "../modles/user.models.js";
+import { ApiResponse } from "../utils/api_response.js";
+import { ApiError } from "../utils/api_error.js";
+import { asyncHandler } from "../utils/async_handler.js";
 
-// const registerUser = asyncHandler(asyn (req, res) => {
-//   const{email,username,password,role} = req.body
-  
-//   const existedUser = await User.findOne({
-//     $or: [{username}, {email}]
-//   })
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
 
-//   if (existedUser){
-//     throw new ApiError(409, "User with email or username already exists")
-//   }
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
 
-//   const user =await user.create ({
-//     email,
-//     password,
-//     Username,
-//     isEmailVerfied: false 
-//   })
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
 
-// })
+  const user = await User.create({
+    email,
+    username,
+    password,
+    role,
+    isEmailVerified: false,
+  });
+
+  return res.status(201).json(
+    new ApiResponse(201, { user }, "User registered successfully")
+  );
+});
+
+export { registerUser };
