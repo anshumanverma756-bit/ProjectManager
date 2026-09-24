39b4ee29fce62093b75b41b4d5d5c79812f6c903
anshu <anshumanverma756@gmail.com>
2026-09-25 00:18:10 +0530

    registered the user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,23 +2,22 @@ import { User } from "../modles/user.models.js";
 import { ApiResponse } from "../utils/api_response.js";
 import { ApiError } from "../utils/api_error.js";
 import { asyncHandler } from "../utils/async_handler.js";
+import { sendEmail } from "../utiles/mail.js";
+import { emailVerificationMailgenContent } from "../uitles/mail.js";
 
 const generateAccessAndRefreshToken = async (userID) => {
-  try{
-    const user = await User.findById(userID)
+  try {
+    const user = await User.findById(userID);
     const accessToken = user.generateAccessToken();
     const refreshToken = user.generateRefreshToken();
 
-    user.refreshToken = refreshToken
-    await user.save({validateBeforeSave: false})
-    return {accessToken, refreshToken}
-  }catch (error){
-
-    500,
-    "Something went wrong while generting access token "
-  };
-
-}
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, "Something went wrong while generating access token");
+  }
+};
 
 const registerUser = asyncHandler(async (req, res) => {
   const { email, username, password, role } = req.body;
@@ -30,7 +29,7 @@ const registerUser = asyncHandler(async (req, res) => {
   if (existedUser) {
     throw new ApiError(409, "User with email or username already exists");
   }
-  
+
   const user = await User.create({
     email,
     username,
@@ -38,13 +37,37 @@ const registerUser = asyncHandler(async (req, res) => {
     role,
     isEmailVerified: false,
   });
- const {unHashedToken, hashedToken, tokenExpiry} = user.generateTemporaryToken();
-  
 
- user.emailVerificationToken = hashedToken
- user.emailVerificationexpiry = tokenExpiry
+  const { unHashedToken, hashedToken, tokenExpiry } = user.generateTemporaryToken();
 
- await user.save({validateBeforeSave: false})
+  user.emailVerificationToken = hashedToken;
+  user.emailVerificationexpiry = tokenExpiry;
+
+  await user.save({ validateBeforeSave: false });
+
+  await sendEmail({
+    email: user?.email,
+    subject: "Please verify your email",
+    mailgenContent: emailVerificationMailgenContent(
+      user.username,
+      `${req.protocol}://${req.get("host")}/api/v1/users/verify-email/${unHashedToken}`
+    ),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+  );
+  if(!createdUser){
+    throw new ApiError(500, "Something went wrong while registering a user")
+  }
+  return res
+  .status(201)
+  .json(
+    new ApiResponse(
+      200,
+      {user: createdUser} ,"User registered successfully and verifiaction email has been sent on your email "
+    ),
+  )
 });
 
-export { registerUser };
+export {registerUser};
